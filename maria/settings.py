a9e772f2764ac43ef537987fba38bc22b30ef5f1
Arham Javed <arhamjaved46@gmail.com>
2022-09-14 18:19:45 +0500

    inital commit

--- maria/settings.py
+++ maria/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x@ejm0c(ka3k5mg*p0aj^l(fayu%zu_t6855xdnob*b-qei#$)'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
