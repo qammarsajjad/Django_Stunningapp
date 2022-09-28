652f2bd4833ced8678f8a07f31f663fa57b8c491
Arham Javed <arhamjaved46@gmail.com>
2022-09-28 12:52:28 +0500

    message

--- maria/settings.py
+++ maria/settings.py
@@ -1,5 +1,6 @@
 import os
 import django_on_heroku
+import dj_database_url
 
 
 """
@@ -138,8 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 
 STATIC_URL = '/static/'
 
@@ -147,6 +146,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT =os.path.join(BASE_DIR,'media')
 MEDIA_URL ='/media/'
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
