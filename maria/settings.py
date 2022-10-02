9519e1d8c2183ae2952e116a28eb49f54305eb1f
Arham Javed <arhamjaved46@gmail.com>
2022-10-02 16:56:54 +0500

    Updated

--- maria/settings.py
+++ maria/settings.py
@@ -14,6 +14,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '757e584c5dc5d1369f351b631c607dbf192718ac6cfcfa53'
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
